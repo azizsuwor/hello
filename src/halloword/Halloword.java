a5585ad24da78ff10ed58f5e3bcbae56e7ab3b1f
lenovo <lenovo@DESKTOP-T41SDQR>
2024-03-13 12:34:32 -0800

    add helo word

--- src/halloword/Halloword.java
+++ src/halloword/Halloword.java
@@ -15,6 +15,7 @@ public class Halloword {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("helo word");
     }
     
 }
